f5293d4c79c71deee5fced2c39f8d9e75ef9b811
xaviguasch <xaviguasch@gmail.com>
2018-10-07 18:52:40 +0200

    adding error handling logic with try / catch

--- todo-functions.js
+++ todo-functions.js
@@ -3,10 +3,15 @@
 
 // Fetch existing todos from localStorage
 const getSavedTodos = () => {
-
   const todosJSON = localStorage.getItem('todos')
 
-  return (todosJSON) ? JSON.parse(todosJSON) : []
+  try {
+    return (todosJSON) ? JSON.parse(todosJSON) : []
+  } catch (e) {
+    return []
+  } 
+
+
 
 }
 
