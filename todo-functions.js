ef39ce97668dc8f7e60cadf5b49672d383178ba9
xaviguasch <xaviguasch@gmail.com>
2018-10-07 15:13:59 +0200

    refactoring with ternary operators

--- todo-functions.js
+++ todo-functions.js
@@ -6,11 +6,8 @@ const getSavedTodos = () => {
 
   const todosJSON = localStorage.getItem('todos')
 
-  if (todosJSON !== null) {
-    return JSON.parse(todosJSON)
-  } else {
-    return []
-  }
+  return (todosJSON !== null) ? JSON.parse(todosJSON) : []
+
 }
 
 // Save todos to localStorage
