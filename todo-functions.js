b6d5eb3cfde4ea6db607954f2b0e8cef0ec7fa01
xaviguasch <xaviguasch@gmail.com>
2018-10-07 16:02:38 +0200

    refactoring with truthy and falsy operators

--- todo-functions.js
+++ todo-functions.js
@@ -6,7 +6,7 @@ const getSavedTodos = () => {
 
   const todosJSON = localStorage.getItem('todos')
 
-  return (todosJSON !== null) ? JSON.parse(todosJSON) : []
+  return (todosJSON) ? JSON.parse(todosJSON) : []
 
 }
 
@@ -30,7 +30,7 @@ const removeTodo = (id) => {
 const toggleTodo = (id) => {
   const todo = todos.find((todo) => todo.id === id)
   
-  if (todo !== undefined) {
+  if (todo) {
     todo.completed = !todo.completed
   }
   
